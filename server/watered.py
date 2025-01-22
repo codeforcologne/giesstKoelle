16783c951a3894aa1f3a11a8ce6f4964f1310fa7
OpenDEM <contact@opendemdata.info>
2025-01-22 20:34:57 +0100

    Update watered.py
    use Python-dotenv for security reasons

--- server/watered.py
+++ server/watered.py
@@ -3,6 +3,11 @@ import cgi
 import json
 import psycopg2
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os 
+
+load_dotenv('/home/python_env/config.env')
+
 try:
     date_object = datetime.today()
     date_string = date_object.strftime("%Y-%m-%d")
@@ -10,9 +15,15 @@ try:
     uuid = arguments.getvalue("id")
     liter = arguments.getvalue("liter")
     comment = arguments.getvalue("comment")
-    conn = psycopg2.connect("host=yourHost dbname=yourDB user=yourUser password=yourPassword")
+    conn = psycopg2.connect(
+       user = os.getenv("DATABASE_USERNAME"),
+       password = os.getenv("DATABASE_PASSWORD"),
+       host = os.getenv("DATABASE_IP"),
+       port = os.getenv("DATABASE_PORT"),
+       database = os.getenv("DATABASE_NAME")
+    )
     cur = conn.cursor()
-    cur.execute("UPDATE trees SET watered_at = %s, watered = %s,comment = %s WHERE tree_id = %s", (date_string, liter, comment ,uuid))
+    cur.execute("UPDATE trees_3857 SET watered_at = %s, watered = %s,comment = %s WHERE tree_id = %s", (date_string, liter, comment ,uuid))
     conn.commit()
     cur.close()
     conn.close()
